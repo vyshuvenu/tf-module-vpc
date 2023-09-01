5da06ed1499606b0e92cf4ef7b895a57c81f2f62
vyshuvenu <vyshuv1289@gmail.com>
2023-09-01 11:51:50 +0530

    new commit

--- vars.tf
+++ vars.tf
@@ -1,1 +1,7 @@
-variable "cidr {"
+variable "cidr" {}
+variable "subnets" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_route_table_id" {}
+variable "tags" {}
+variable "env" {}
